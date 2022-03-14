23c1b7e34c78d08524becb7a56d635ec3ef4ca7f
VQ9 <nonososoviolin@gmail.com>
2022-03-14 23:07:19 +0000

    a

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from tkinter import Tk, Label
 import asynctkinter as at
+import RPIO as GPIO
+import motor
+import steering
+import bluetooth
+import time
 
 
 class Main:
@@ -7,10 +12,43 @@ class Main:
     def __init__(self):
         print("Bish project starting up")
 
-        print("Starting sensors")
+        print("Starting motors")
 
+        self.motor1 = motor.Motor(7, 11)
+        self.motor2 = motor.Motor(8, 10)
+        self.controls = steering.Steering(self.motor1, self.motor2)
+        self.startControl()
 
-    def initSensors(self):
+    def startControl(self):
         print("sensor init stuff")
+
+        serverMACAddress = '00:1f:e1:dd:08:3d'
+        port = 3
+        size = 1024
+        s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        s.bind((serverMACAddress, port))
+        s.listen(1)
+        try:
+            client, clientInfo = s.accept()
+            while 1:
+                data = client.recv(size)
+                if data == "quit":
+                    break
+                elif data == "forward":
+                    self.controls.forward()
+                elif data == "backwards":
+                    self.controls.backward()
+                elif data == "left":
+                    self.controls.left()
+                elif data == "right":
+                    self.controls.right()
+                elif data == "stop":
+                    self.controls.stop()
+                client.send(data)
+        except:
+            print("closing")
+            client.close()
+            s.close()
+
 
 Main()
---
+++ motor.py
@@ -0,0 +1,24 @@
+class Motor:
+    def __init__(self, F, B):
+        self.pinFwd = F
+        self.pinBwd = B
+
+        GPIO.setmode(GPIO.BOARD)  # Sets the pins to use the Board Numbering
+
+        GPIO.setup(self.pinFwd, GPIO.OUT)  # Sets pin 1
+        GPIO.setup(self.pinBwd, GPIO.OUT)  # Sets pin 2
+
+    def __del__(self):
+        GPIO.cleanup()
+
+    def forward(self):
+        GPIO.output(self.pinFwd, GPIO.HIGH)
+        GPIO.output(self.pinBwd, GPIO.LOW)
+
+    def backward(self):
+        GPIO.output(self.pinFwd, GPIO.LOW)
+        GPIO.output(self.Bwd, GPIO.HIGH)
+
+    def stop(self):
+        GPIO.output(self.pinFwd, GPIO.LOW)
+        GPIO.output(self.pinBwd, GPIO.LOW)
