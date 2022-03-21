c07a2b44a60e1845f2c97567721f74a52beeb9c0
VQ9 <nonososoviolin@gmail.com>
2022-03-21 20:59:27 +0000

    Final Code

--- motor.py
+++ motor.py
@@ -1,29 +1,34 @@
+import RPi.GPIO as GPIO
 
 
-# hi
+def clean():
+    GPIO.cleanup()
 
 
 class Motor:
-    def __init__(self, F, B):
-        self.pinFwd = F
-        self.pinBwd = B
+    def __init__(self, in1, in2, en):
+        self.en = en
+        self.in1 = in1
+        self.in2 = in2
 
-        GPIO.setmode(GPIO.BOARD)  # Sets the pins to use the Board Numbering
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(True)
+        GPIO.setup(in1, GPIO.OUT)
+        GPIO.setup(in2, GPIO.OUT)
+        GPIO.setup(en, GPIO.OUT)
+        self.p = GPIO.PWM(en, 1000)
 
-        GPIO.setup(self.pinFwd, GPIO.OUT)  # Sets pin 1
-        GPIO.setup(self.pinBwd, GPIO.OUT)  # Sets pin 2
-
-    def __del__(self):
-        GPIO.cleanup()
+        self.p.start(100)
+        self.p.ChangeDutCycle(75)
 
     def forward(self):
-        GPIO.output(self.pinFwd, GPIO.HIGH)
-        GPIO.output(self.pinBwd, GPIO.LOW)
+        GPIO.output(self.in1, GPIO.HIGH)
+        GPIO.output(self.in2, GPIO.LOW)
 
     def backward(self):
-        GPIO.output(self.pinFwd, GPIO.LOW)
-        GPIO.output(self.Bwd, GPIO.HIGH)
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.HIGH)
 
     def stop(self):
-        GPIO.output(self.pinFwd, GPIO.LOW)
-        GPIO.output(self.pinBwd, GPIO.LOW)
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.LOW)
